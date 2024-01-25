65584af66b5385fdbbfe410f7951d59dd6eb5fb5
dev1d123 <139636102+dev1d123@users.noreply.github.com>
2024-01-24 21:07:05 -0500

    Corrección tarjeta.pl

--- cgi-bin/tarjeta.pl
+++ cgi-bin/tarjeta.pl
@@ -10,27 +10,28 @@ my $database = 'proyecto_pweb1';
 my $host     = 'localhost';
 my $port     = '3306'; 
 my $user     = 'root';
-my $password = '';
+my $passwor = ''; 
 
 my $cgi = CGI->new;
-my $creationDate = $cgi->param('creationDate');
-my $dueDate = $cgi->param('dueDate');
-my $password = $cgi->param('password');
 
 my $num_tarjeta = int(rand(9000000000000000)) + 1000000000000000;
-#print "El numero es: $num_tarjeta";
 
-my $dbh = DBI->connect("DBI:mysql:database=$database;host=$host;port=$port", $user, $password);
+my $creationDate = $cgi->param('creationDate');
+my $dueDate      = $cgi->param('dueDate');
+my $password     = $cgi->param('password');
+
+my $dbh = DBI->connect("DBI:mysql:database=$database;host=$host;port=$port", $user, $passwor);
 
 unless ($dbh) {
     die "Error de conexión: " . $DBI::errstr;
 }
 
-my $query = "SELECT * FROM tarjetas WHERE numero= ? AND clave = ? AND creado = ? AND vencimiento = ?";
-my $sth = $dbh->prepare($query);
-$sth->execute($num_tarjeta, $password, $creationDate, $dueDate);
+my $insert_query = "INSERT INTO tarjetas (numero, clave, creado, vencimiento) VALUES (?, ?, ?, ?)";
+my $insert_sth = $dbh->prepare($insert_query);
+$insert_sth->execute($num_tarjeta, $password, $creationDate, $dueDate);
 
-#Para poner el estado y sellado de la tarjeta podría ponerse como default el valor true desde la misma
-#base de datos
+if ($insert_sth->errstr) {
+    die "Error al insertar en la base de datos: " . $insert_sth->errstr;
+}
 
 $dbh->disconnect();
