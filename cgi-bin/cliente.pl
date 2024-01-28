d83f74830f5aaea56a4d3214d73ebf4d3545fd0f
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-01-28 15:18:32 -0500

    correccion clientepl

--- cgi-bin/cliente.pl
+++ cgi-bin/cliente.pl
@@ -2,36 +2,44 @@
 use strict;
 use warnings;
 use CGI;
-use CGI::Carp qw(croak fatalsToBrowser);
-use Encode;
 use DBI;
-
-my $database = 'proyecto_pweb1';
-my $host     = 'localhost';
-my $port     = '3306'; 
-my $user     = 'root';
-my $passwor = '';
-
+use DateTime;
+use CGI::Carp qw(croak fatalsToBrowser);
 my $cgi = CGI->new;
 
-my $nombres    = $cgi->param('nombres');
-my $apellidos  = $cgi->param('apellidos');
-my $dni        = $cgi->param('dni');
-my $celular    = $cgi->param('celular');
-my $correo     = $cgi->param('correo');
-my $id_user    = $cgi->param('id_user');
-my $nacimiento = $cgi->param('nacimiento');
-my $genero     = $cgi->param('genero');
+my $servername = "127.0.0.1";
+my $username   = "root";
+my $password   = "Amomifamilia3";
+my $database   = "proyecto_pweb1";
 
-my $dbh = DBI->connect("DBI:mysql:database=$database;host=$host;port=$port", $user, $passwor);
+my $dbh = DBI->connect("DBI:mysql:database=$database;host=$servername", $username, $password, { RaiseError => 1, PrintError => 0 });
 
-unless ($dbh) {
-    die "Error de conexión: " . $DBI::errstr;
+if (!$dbh) {
+    die("Connection failed: " . $DBI::errstr);
 }
 
-my $cliente_insert = $dbh->prepare("INSERT INTO clientes (dni, nombres, paterno, nacimiento, usuario_id) VALUES (?, ?, ?, ?, ?)");
-$cliente_insert->execute($dni, $nombres, $apellidos, $nacimiento, $id_user);
+# Obtener valores desde el formulario HTML
+my $nombres     = $cgi->param('nombres');
+my $paterno     = $cgi->param('a_paterno');
+my $materno     = $cgi->param('a_materno');
+my $dni         = $cgi->param('dni');
+my $nacimiento  = $cgi->param('nacimiento');
+my $creado      = DateTime->now->ymd;
+my $estado      = 1;
+my $usuario     = $cgi->param('usuario');
+my $usuario_id  = 2;
 
-$dbh->disconnect;
+# Preparar la consulta de inserción
+my $insert_query = "INSERT INTO clientes (nombres, paterno, materno, dni, nacimiento, creado, estado, usuario_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+my $insert_sth   = $dbh->prepare($insert_query);
+
+# Ejecutar la consulta de inserción
+$insert_sth->execute($nombres, $paterno, $materno, $dni, $nacimiento, $creado, $estado, $usuario_id);
+
+if ($DBI::errstr) {
+    die("Error al insertar en la base de datos: " . $DBI::errstr);
+}
+
+$dbh->disconnect();
 
 print $cgi->redirect('../confirmacion.html');
