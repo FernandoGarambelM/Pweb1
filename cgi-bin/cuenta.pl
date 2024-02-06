6dfaf0af3f418f3bbd0f194fc8315157bdeea54c
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-02-05 21:55:05 -0500

    cuenta.pl

--- cgi-bin/cuenta.pl
+++ cgi-bin/cuenta.pl
@@ -14,7 +14,7 @@ my $cgi = CGI->new;
 my $tipoMoneda    = $cgi->param('tipo_moneda');
 my $titular       = $cgi->param('id_titular');
 my $usuario       = $cgi->param('id_usuario');
-
+my $tarjetaId = 1;
 # Conexión a la base de datos
 my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_password, { RaiseError => 1 });
 
@@ -26,15 +26,6 @@ if (!$dbh) {
 # Generar el número de cuenta
 my $numero = int(rand(9000000000000000)) + 1000000000000000;
 
-# Verificar si el cliente ya tiene una cuenta con el mismo número
-my $verificacion_numero = $dbh->prepare("SELECT id FROM cuentas WHERE numero = ?");
-$verificacion_numero->execute($numero);
-
-if ($verificacion_numero->fetchrow_array) {
-    mostrar_mensaje("El número de cuenta $numero ya está registrado.");
-    $dbh->disconnect;
-    exit;
-}
 # Verificar si el cliente ya tiene una cuenta
 my $verificacion_cuenta = $dbh->prepare("SELECT id FROM cuentas WHERE cliente_id = ?");
 $verificacion_cuenta->execute($titular);
@@ -53,10 +44,23 @@ if (!$verificacion_usuario->fetchrow_array) {
     exit;
 }
 
+
 # Realizar la inserción en la tabla 'cuentas'
 my $cuenta_insert = $dbh->prepare("INSERT INTO cuentas (numero, moneda, tarjeta_id, cliente_id, usuario_id) VALUES (?, ?, ?, ?, ?)");
-$cuenta_insert->execute($numero, $tipoMoneda, 1, $titular, $usuario);
+$cuenta_insert->execute($numero, $tipoMoneda, $tarjetaId, $titular, $usuario);
 
+#obtener el id de la cuenta
+my $get_id_cuenta = $dbh->prepare("SELECT id FROM cuentas WHERE numero = ?");
+$get_id_cuenta->execute($numero);
+
+my $id_cuenta = $get_id_cuenta->fetchrow_array;
+if (!$id_cuenta) {
+    mostrar_mensaje("Error al obtener el ID de la cuenta recién creada.");
+    $dbh->disconnect;
+    exit;
+}
+my $movimiento_insert = $dbh->prepare("INSERT INTO movimientos (tarjeta_id, cuenta_id, monto, tipo) VALUES (?, ?, ?, ?)");
+$movimiento_insert->execute($tarjetaId, $id_cuenta, 0, 1);
 mostrar_mensaje("Inserción exitosa en la base de datos.");
 
 $dbh->disconnect;
